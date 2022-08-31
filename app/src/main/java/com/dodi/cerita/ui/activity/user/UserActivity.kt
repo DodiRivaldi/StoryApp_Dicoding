2e2f6bc6aff992648b29233b578157d65c63eaa0
BMJAKARTA\drivaldi <dodi.rivaldi@linknet.co.id>
2022-08-31 16:42:20 +0700

    add ui signup

--- app/src/main/java/com/dodi/cerita/ui/activity/user/UserActivity.kt
+++ app/src/main/java/com/dodi/cerita/ui/activity/user/UserActivity.kt
@@ -3,10 +3,15 @@ package com.dodi.cerita.ui.activity.user
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dodi.cerita.R
+import com.dodi.cerita.databinding.ActivityUserBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class UserActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityUserBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user)
+        binding = ActivityUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/dodi/cerita/ui/fragment/signin/SignInFragment.kt
+++ app/src/main/java/com/dodi/cerita/ui/fragment/signin/SignInFragment.kt
@@ -1,10 +1,11 @@
-package com.dodi.cerita
+package com.dodi.cerita.ui.fragment.signin
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.dodi.cerita.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
